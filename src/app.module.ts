e49fb97277f476736bb6274a2e483205d02e39ac
Humberto Fernández <humbertof44@gmail.com>
2024-05-12 08:06:26 -0400

    Add necessary modules, controllers and services to our API

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
 import config from './ormconfig';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(config),
+    UsersModule,
+    ProductsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
